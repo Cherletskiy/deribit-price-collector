cd64d15dc96a8ec76d736b2cc79097b539cf7bd5
Cherletskiy <cherletskiy.vladimir@yandex.ru>
2026-01-20 15:50:25 +0000

    feat: optimized indexes (composite index for ticker+timestamp)

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from datetime import datetime
-from sqlalchemy import func
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Numeric
 from decimal import Decimal
+
+from sqlalchemy import Index, Numeric, func
+from sqlalchemy.orm import Mapped, mapped_column
+
 from app.core.db import Base
 
 
@@ -10,10 +11,18 @@ class Price(Base):
     __tablename__ = "prices"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    ticker: Mapped[str] = mapped_column(nullable=False, index=True)
-    price: Mapped[Decimal] = mapped_column(Numeric(precision=20, scale=8), nullable=False)
-    timestamp: Mapped[int] = mapped_column(nullable=False, index=True)
-    created_at: Mapped[datetime] = mapped_column(server_default=func.now(), nullable=False)
 
-    def __repr__(self):
-        return f"<Price(ticker='{self.ticker}', price={self.price}, timestamp={self.timestamp}, created_at={self.created_at})>"
+    ticker: Mapped[str] = mapped_column(nullable=False)
+    price: Mapped[Decimal] = mapped_column(
+        Numeric(precision=20, scale=8),
+        nullable=False,
+    )
+
+    timestamp: Mapped[int] = mapped_column(nullable=False)
+
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        nullable=False,
+    )
+
+    __table_args__ = (Index("ix_prices_ticker_timestamp", "ticker", "timestamp"),)
